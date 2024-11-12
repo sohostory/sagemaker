595367d3c94048eb2d97cdf3ad4755c668dc0f7d
sohostory <sohostory@gmail.com>
2024-11-12 18:26:37 +0000

    training script

--- script.py
+++ script.py
@@ -39,3 +39,79 @@ def encode_cat(x):
     return encode_dict[x]
 
 df['ENCODE_CAT'] = df['CATEGORY'].apply(lambda x: encode_cat(x))
+
+# resets the index of a Dataframe, which mens it creatse a new range index starting from 0 to len(df)-1, old indexes are dropped
+df = df.reset_index(drop=True)
+
+tokenizer = DistilBertTokenizer.from_pretrained('distibert-base-uncased')
+
+class NewsDataset(Dataset):
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.len = len(dataframe)
+        self.data = dataframe
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        
+    def _getitem__(self, index):
+        # uses integer-based indexing meaning index is treated as a position in the df. 0 selects the row at the specified index, TITLE column
+        title = str(self.data.iloc[index, 0])
+        title = " ".join(title.split())
+        inputs = self.tokenizer.encode_plus(
+            title,
+            None,
+            add_special_token = True,
+            max_length = self.max_len,
+            padding = 'max_length',
+            return_token_type_ids = True,
+            truncation = True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        
+        return {
+            'ids': torch.tensor(ids, dtype = torch.long)
+            'mask': torch.tensor(mask, dtype = torch.long)
+            'targets': torch.tensor(self.date.iloc[index, 2], dtype = torch.long)  # same thing with iloc here, 2 means we access column 3
+        }
+    
+    def __len__(self):
+        return self.len
+    
+
+    train_size = 0.8
+    train_dataset = df.sample(frac=train_size,random_state=200)
+    test_dataset = df.drop(train_dataset.index).reset_index(drop=True)
+
+    train_dataset.reset_index(drop=True)
+
+
+    print("Full dataset: {}".format(df.shape))
+    print("Train dataset: {}".format(train_dataset.shape))
+    print("Test dataset: {}".format(test_dataset.shape))
+
+
+    MAX_LEN = 512
+    TRAIN_BATCH_SIZE = 4
+    VALID_BATCH_SIZE = 2
+
+
+
+    training_set = NewsDataset(train_dataset,tokenizer,MAX_LEN)
+    testing_set = NewsDataset(test_dataset,tokenizer,MAX_LEN)
+
+
+    train_parameters = {
+                        'batch_size':TRAIN_BATCH_SIZE,
+                        'shuffle':True,
+                        'num_workers':0
+                        }
+    test_parameters = {
+                        'batch_size':VALID_BATCH_SIZE,
+                        'shuffle':True,
+                        'num_workers':0
+                        }
+
+
+    training_loader = DataLoader(training_set, **train_parameters)
+    testing_loader = DataLoader(testing_set, **test_parameters)
+    
